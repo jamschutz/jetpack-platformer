43cb6b70652ee5428e79116f1b42e4eca32e9461
joey schutz <schutzjoey@gmail.com>
2026-01-20 19:01:56 -0500

    switching from rigidbody character controller -- have movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,103 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // public
-    public float speed;
+    // =============================================================== //
+    // ========  Variables  ========================================== //
+    // =============================================================== //
 
-    // components
-    private Rigidbody2D rb;
+    // ------------ public ----------- //
+    public float speed = 9;
+    public float walkAcceleration = 75;
+    public float airAcceleration = 30;
+    public float groundDeceleration = 70;
+    public float jumpHeight = 4;
 
 
-    private void Start()
+
+    // ------------ private ----------- //
+    private BoxCollider2D collider;
+    private Vector2 velocity;
+
+    // helpers
+    private readonly KeyCode[] leftKeys = { KeyCode.A, KeyCode.LeftArrow };
+    private readonly KeyCode[] rightKeys = { KeyCode.D, KeyCode.RightArrow };
+    private readonly KeyCode[] jumpKeys = { KeyCode.W, KeyCode.UpArrow, KeyCode.Space };
+
+
+
+
+
+    // =============================================================== //
+    // ========  Lifecycle Methods  ================================== //
+    // =============================================================== //
+
+    private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        collider = GetComponent<BoxCollider2D>();
     }
 
 
     private void Update()
     {
-        void Move(Vector2 dir)
-        {
-            rb.linearVelocity += dir * speed;
+        Move();
+    }
+
+
+
+
+
+    // =============================================================== //
+    // ========  Main Methods  ======================================= //
+    // =============================================================== //
+
+    private void Move()
+    {
+        // read input
+        float moveInput = GotInput(leftKeys)?  -1.0f : 0.0f;
+        moveInput +=      GotInput(rightKeys)?  1.0f : 0.0f;
+
+        // if we got move input, accelerate towards top speed
+        if(moveInput != 0) {
+            velocity.x = Mathf.MoveTowards(velocity.x, speed * moveInput, walkAcceleration * Time.deltaTime);
+        }
+        // otherwise, decelerate to zero
+        else {
+            velocity.x = Mathf.MoveTowards(velocity.x, 0, groundDeceleration * Time.deltaTime);
         }
 
-        // reset movement
-        rb.linearVelocity = Vector2.zero;
+        transform.Translate(velocity * Time.deltaTime);
+    }
 
-        // get input, and move
-        if(Input.GetKey(KeyCode.A)) Move(Vector2.left);
-        if(Input.GetKey(KeyCode.D)) Move(Vector2.right);
+
+    private void Jump()
+    {
+
+    }
+    
+
+
+
+
+
+    // =============================================================== //
+    // ========  Helper Methods  ===================================== //
+    // =============================================================== //
+
+
+    private bool GotInput(KeyCode[] input, bool getKeyDown = false)
+    {
+        for(int i = 0; i < input.Length; i++) {
+            if(getKeyDown) {
+                if(Input.GetKeyDown(input[i]))
+                    return true;
+            }
+            else if(Input.GetKey(input[i])) {
+                return true;
+            }
+        }
+
+        return false;
     }
 }
